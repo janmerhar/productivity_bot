2d035d57d4296a86281d04aded2786b0c67f453e
Jan Merhar <merharjan@gmail.com>
2021-07-16 21:20:37 +0200

    Added clear command

--- main.py
+++ main.py
@@ -8,4 +8,12 @@ client = commands.Bot(command_prefix = ".")
 async def on_ready():
     print("Bot is ready.")
 
+@client.command()
+async def ping(ctx):
+    await ctx.send(f"Pong! {round (client.latency * 1000)}ms")
+
+@client.command()
+async def clear(ctx, ammount=5):
+    await ctx.channel.purge(limit=ammount)
+
 client.run("ODY1NTUyMjI0ODI1MjQ1Njk3.YPFqUw.rOrPem7DDCrbTV2CPr_wind0Ec8")
