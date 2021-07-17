a16dc227499ff57d5b081f44728b9026fd2fd43d
Jan Merhar <merharjan@gmail.com>
2021-07-17 16:07:26 +0200

    Better exapmle withj Cogs

--- cogs/example.py
+++ cogs/example.py
@@ -2,11 +2,19 @@ import discord
 from discord.ext import commands
 import os
 
-class Example(command.Cog):
+class Example(commands.Cog):
     def __init__(self, client):
         self.client = client
     
-
+    # Events
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Bot is ready to rumble")
+    
+    # Commands
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send(f"Pong! {round (self.client.latency * 1000)}ms")
 
 def setup(client):
     client.add_cog(Example(client))
--- main.py
+++ main.py
@@ -16,15 +16,18 @@ async def unload(ctx, extension):
 for filename in os.listdir("./cogs"):
     if filename.endswith(".py"):
         client.load_extension(f"cogs.{filename[:-3]}")
-
+"""
 @client.event
 async def on_ready():
     await client.change_presence(status=discord.Status.idle, activity=discord.Game("Playing Google Calendar"))
     print("Bot is ready.")
+"""
 
+"""
 @client.command()
 async def ping(ctx):
     await ctx.send(f"Pong! {round (client.latency * 1000)}ms")
+"""
 
 @client.command()
 async def clear(ctx, ammount=5):
