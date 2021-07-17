83ba8c2e3215d49e0cbadcd91c496078604edda9
Jan Merhar <merharjan@gmail.com>
2021-07-17 16:23:42 +0200

    Added botinfo into Cog

--- cogs/example.py
+++ cogs/example.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 import os
 
+import platform
+
 class Example(commands.Cog):
     def __init__(self, client):
         self.client = client
@@ -12,19 +14,31 @@ class Example(commands.Cog):
         print("Bot is ready to rumble")
     
     # Commands
-    """
-    @commands.command()
-    async def ping(self, ctx):
-        await ctx.send(f"Pong! {round (self.client.latency * 1000)}ms")
-    """
-    
-    # Commands
     @commands.command()
     async def ping(self, ctx):
         embed = discord.Embed(
             title="🏓 Pong!",
             description=f"The bot latency is {round(self.client.latency * 1000)}ms.",
             color=0x42F56C
+        )
+        await ctx.send(embed=embed)
+
+    @commands.command(aliases=["botinfo"])
+    async def info(self, ctx):
+        embed = discord.Embed(
+            description="Productivity bot",
+            color=0x00FF00
+        )
+        embed.set_author(
+            name="About this bot"
+        )
+        embed.add_field(
+            name="Prefix:",
+            value=".",
+            inline=False
+        )
+        embed.set_footer(
+            text=f"Requested by {ctx.message.author}"
         )
         await ctx.send(embed=embed)
 
