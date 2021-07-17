054c8e2c4ebb23d1c2cfaec3922dc2120fbe8b2a
Jan Merhar <merharjan@gmail.com>
2021-07-17 16:14:13 +0200

    Added ping command in Cog

--- cogs/example.py
+++ cogs/example.py
@@ -12,9 +12,21 @@ class Example(commands.Cog):
         print("Bot is ready to rumble")
     
     # Commands
+    """
     @commands.command()
     async def ping(self, ctx):
         await ctx.send(f"Pong! {round (self.client.latency * 1000)}ms")
+    """
+    
+    # Commands
+    @commands.command()
+    async def ping(self, ctx):
+        embed = discord.Embed(
+            title="🏓 Pong!",
+            description=f"The bot latency is {round(self.client.latency * 1000)}ms.",
+            color=0x42F56C
+        )
+        await ctx.send(embed=embed)
 
 def setup(client):
     client.add_cog(Example(client))
