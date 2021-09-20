a5995a69ffe8279085aafc21473ef8ccfa2b294c
Jan Merhar <merharjan@gmail.com>
2021-09-20 14:15:56 +0200

    Added Event parser for Google Calendar

--- cli_args/EventGetParser.py
+++ cli_args/EventGetParser.py
@@ -12,3 +12,33 @@ get_events(
 ) → Iterable[gcsa.event.Event]
 """
 event_get = argparse.ArgumentParser()
+
+# start of the search range
+event_get.add_argument("-s",
+                        "--start",
+                        action="append",  
+                        nargs="+",        
+                        required=True, 
+                        help="Start of the search range"
+)
+# end of the search range
+event_get.add_argument("-e",
+                        "--end",
+                        action="append",  
+                        nargs="+",        
+                        required=True, 
+                        help="End of the search range"
+)
+
+# Query that is searched in event fields
+event_get.add_argument("-q",
+                       "--query",
+                       action="append", 
+                       nargs="*", 
+                       help="Query that is searched in event fields"
+)
+
+if __name__ == "__main__":
+    args = event_get.parse_args("-s today -e tomorrow -q 1 2 3 4 5 6 7 8 9".split())
+    args = vars(args)
+    print(args)
