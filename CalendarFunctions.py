1a450f2d912a6f6df8c7a6e24b1864db063e7a80
Jan Merhar <merharjan@gmail.com>
2021-09-20 16:14:25 +0200

    Improved eventToObject method with better submethods

--- CalendarFunctions.py
+++ CalendarFunctions.py
@@ -29,15 +29,35 @@ class CalendarFunctions:
         
         return pdtCal.parseDT(date, now)
 
+    def createRemindersObject2(self, parsedString):
+        if  parsedString["reminders"] is None:
+            return None
+        reminders = []
+        for reminder in parsedString["reminders"][0]:
+            reminders.append(PopupReminder(reminder))
+        
+        return reminders
+
+    def createDatetimeObject2(self, parsedString):
+        pdtCal = pdt.Calendar()
+        now = datetime.now()
+
+        start = pdtCal.parseDT(" ".join(parsedString["start"][0]), now)[0]
+        end = pdtCal.parseDT(" ".join(parsedString["end"][0]), now)[0]
+        return [start, end]
+
+    def createDescriptionObject2(self, parsedString):
+        return " ".join(parsedString["description"][0]) if parsedString["description"] else None
+    
     def eventToObject(self, parsedString):
         eventOptions = parsedString
-        eventOptions["reminders"] = self.createRemindersObject(parsedString["reminders"][0]) if parsedString["reminders"] else None
-        eventOptions["start"] = self.createDatetimeObject(" ".join(parsedString["start"][0]))[0]
-        eventOptions["end"] = self.createDatetimeObject(" ".join(parsedString["end"][0]))[0]
-        eventOptions["description"] = " ".join(parsedString["description"][0]) if parsedString["description"] else None
+        eventOptions["reminders"] = self.createRemindersObject2(parsedString)
+        eventOptions["start"], eventOptions["end"] = self.createDatetimeObject2(parsedString)
+        eventOptions["description"] = self.createDescriptionObject2(parsedString)
 
         # potrebujem nekje handlanje exceptionov v primeru nepravilnih vnesenih podatkov o eventu
         event = Event(**eventOptions)
+        print(eventOptions)
         created_event = self.calendar.add_event(event)
         print(created_event)
         return created_event
@@ -47,8 +67,8 @@ if __name__ == "__main__":
     # calendar_functions.printAllEvents()
     # calendar_functions.createRemindersObject([15, 60, 360])
     # print(calendar.list_event_colors())
-    args = calendar_parser.parse_args("-n ime -s thursday 7 am -e thursday 7 am -c 3".split())
+    args = calendar_parser.parse_args("-n ime -s thursday 7 am -e thursday 8 am -c 3 -r 1 2 3 -d 123<br/>".split())
     args = vars(args)
     # print(**calendar_functions.eventToObject(args))
     # print(" ".join(args["start"][0]))
-    # calendar_functions.eventToObject(args)
+    calendar_functions.eventToObject(args)
