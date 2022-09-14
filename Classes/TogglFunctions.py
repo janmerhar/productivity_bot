307e68b5a0c784755bb3d0f86624c5293ba3971a
Jan Merhar <merharjan@gmail.com>
2022-09-14 22:06:18 +0200

    Add class structure to classes/TogglFunctions.py

--- Classes/TogglFunctions.py
+++ Classes/TogglFunctions.py
@@ -11,13 +11,20 @@ class TogglFunctions:
         self.auth=(API_KEY, "api_token")
 
     # 
+    # Authentication
     # https://developers.track.toggl.com/docs/authentication
     # 
     def aboutMe(self) -> dict:
         res = requests.get('https://api.track.toggl.com/api/v9/me', auth=self.auth)
         return json.loads(res.text)
 
-    
+    # 
+    # Tracking
+    # https://developers.track.toggl.com/docs/tracking
+    # 
+    def startCurrentTimeEntry(self) -> Dict:
+        pass
+
     def getCurrentTimeEntry(self) -> Dict:
         pass
 
@@ -27,6 +34,32 @@ class TogglFunctions:
     def insertTimeEntry(self, entry: dict):
         pass
 
+    def getTimeEntryHistory(self, start_date, end_date):
+        pass
+
+    # 
+    # Workspace
+    # https://developers.track.toggl.com/docs/workspace
+    # 
+
+    def getWorkspaces(self):
+        pass
+
+    # 
+    # Projects
+    # https://developers.track.toggl.com/docs/projects
+    # 
+
+    def createProject(self, data):
+        pass
+
+    def getProjects(self, id=None, workspace=None):
+        pass
+
+    def addEntryToProject(self, data):
+        pass
+    
 if __name__ == "__main__":
     toggl = TogglFunctions(env["TOGGL_TOKEN"])
-    print(toggl.aboutMe())
+    res = toggl.aboutMe()
+    print(res)
