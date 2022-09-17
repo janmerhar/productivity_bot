467c4a6ee023cdcadd137607aaab4893baa98d40
Jan Merhar <merharjan@gmail.com>
2022-09-17 22:13:14 +0200

    Add tasksFromProject(), getProjectByName() and createObject() in classes/TickTickFunctions.py

--- Classes/TickTickFunctions.py
+++ Classes/TickTickFunctions.py
@@ -19,8 +19,6 @@ class TickTickFunctions:
     """
     - builder(self, ...)
     - dates(self, start, due=None, tz=None)
-    - get_completed(self, start, end=None, full=True, tz=None)
-    - get_from_project(self, project)
     - make_subtask(self, obj, parent)
     - move(self, obj, new)
     - move_all(self, old, new)
@@ -42,19 +40,49 @@ class TickTickFunctions:
 
         completed_task = self.client.task.delete(task)
         return completed_task
+
+    def tasksFromProject(self, project_name):
+        project = self.getProjectByName(project_name)
+
+        if project == []:
+            return None
+
+        tasks = self.client.task.get_from_project(project["id"])
+        return tasks
+
     #
     # Projects
     # https://lazeroffmichael.github.io/ticktick-py/usage/projects/
     #
 
     """
-    - create(self, name, color='random', project_type='TASK', folder_id=None) 
     - create_folder(self, name)
     - delete(self, ids)
     - delete_folder(self, ids)
     - update(self, obj) 
     - update_folder(self, obj) 
     """
+
+    """
+    This method wants included emoji with name
+    - e.g. 🚧Projekti
+    """
+
+    def getProjectByName(self, name):
+        project = self.client.get_by_fields(
+            search="projects", name=name)
+        return project
+
+    def createProject(self, name, color='random', project_type='TASK', folder_id=None):
+        search = self.getProjectByName(name)
+
+        if search == []:
+            None
+
+        project = self.client.project.create(
+            name=name, color=color, project_type=project_type, folder_id=folder_id)
+
+        return project
 
     #
     # Tags
@@ -82,10 +110,16 @@ class TickTickFunctions:
 if __name__ == '__main__':
     from dotenv import dotenv_values
     env = dotenv_values(".env")
+    import json
 
     ticktick = TickTickFunctions(
         env["TICK_EMAIL"], env["TICK_PASSWORD"], env["TICK_ID"], env["TICK_SECRET"], env["TICK_URI"])
 
     # res = ticktick.completeTask("test task")
-    res = ticktick.deleteTask("test task")
-    print(res)
+    # res = ticktick.deleteTask("test task")
+    # res = ticktick.tasksFromProject("🍔Hrana")
+    res = ticktick.tasksFromProject("🍔Hrana")
+    # res = ticktick.createProject(name="projekt")
+    # res = ticktick.getProjectByName("🚧Projekti")
+
+    print(json.dumps(res, indent=2))
