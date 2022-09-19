93fba909e2958e62b4748e1950ca17528e4df3f9
Jan Merhar <merharjan@gmail.com>
2022-09-19 03:21:03 +0200

    Add newlist() in cogs/TickTickCog.py

--- cogs/TickTickCog.py
+++ cogs/TickTickCog.py
@@ -231,9 +231,53 @@ class TickTickCog(commands.Cog):
 
             await interaction.response.send_message(embed=embed)
 
+    """
+    Function crashes(raises error) when you try to add a project if there's also maximum limit reachhed
+    """
     @app_commands.command(name="newlist", description="TickTick create new list")
-    async def newlist(self, interaction: discord.Interaction):
-        pass
+    async def newlist(self, interaction: discord.Interaction, name: str, color: str = None, project_type: str = 'TASK', folder_id: str = None):
+        project = self.ticktick.createProject(
+            name=name, color=color, project_type=project_type, folder_id=folder_id
+        )
+
+        # print(project)
+        # await interaction.response.send_message("NI NI")
+        # return
+        if project is None:
+            embed = discord.Embed(
+                title=":ballot_box_with_check: TickTick New Project",
+                color=0xffb301,
+                description="Project already exists"
+            )
+
+            embed.set_thumbnail(
+                url="https://dashboard.snapcraft.io/site_media/appmedia/2022/02/icon_2XdTt7H.png"
+            )
+
+            await interaction.response.send_message(embed=embed)
+        else:
+            # await interaction.response.send_message("Dela")
+            # return
+            embed = discord.Embed(
+                title=":ballot_box_with_check: TickTick New Project",
+                color=discord.Colour.from_str(
+                    project["color"] if project["color"] is not None else "#ffb301")
+            )
+
+            embed.set_thumbnail(
+                url="https://dashboard.snapcraft.io/site_media/appmedia/2022/02/icon_2XdTt7H.png"
+            )
+
+            embed.add_field(
+                name="List ID", value=project["id"], inline=False)
+            embed.add_field(
+                name="List name", value=project["name"], inline=False)
+            embed.add_field(
+                name="List view mode", value=project["viewMode"], inline=False)
+            embed.add_field(
+                name="List kind", value=project["kind"], inline=False)
+
+            await interaction.response.send_message(embed=embed)
 
     @app_commands.command(name="changelist", description="TickTick change list")
     async def changelist(self, interaction: discord.Interaction, identifier: str):
