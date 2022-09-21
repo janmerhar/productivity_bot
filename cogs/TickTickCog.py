1aca3b8f5691e27136290f4b31c49e0e259c9b4c
Jan Merhar <merharjan@gmail.com>
2022-09-21 02:02:39 +0200

    Add newsubtask() in cogs/TickTickCog.py

--- cogs/TickTickCog.py
+++ cogs/TickTickCog.py
@@ -81,8 +81,57 @@ class TickTickCog(commands.Cog):
         await interaction.response.send_message(embed=embed)
 
     @app_commands.command(name="newsubtask", description="TickTick add new subtask")
-    async def newsubtask(self, interaction: discord.Interaction):
-        pass
+    async def newsubtask(self, interaction: discord.Interaction, name: str, parent: str, project_id: str = None, content: str = None, desc: str = None, start_date: str = None, due_date: str = None, time_zone: str = None, reminders: str = None, repeat: str = None, priority: str = None, sort_order: str = None, items: str = None):
+        parent_data = self.ticktick.getTask(parent)
+
+        if parent_data is None:
+            embed = discord.Embed(
+                title=":ballot_box_with_check: TickTick New Task",
+                color="#ffb301",
+                description="Parent not found"
+            )
+
+            embed.set_thumbnail(
+                url="https://dashboard.snapcraft.io/site_media/appmedia/2022/02/icon_2XdTt7H.png"
+            )
+
+            await interaction.response.send_message(embed=embed)
+
+        else:
+
+            task = self.ticktick.createTask(
+                title=name, projectId=parent_data["projectId"], content=content, desc=desc, timeZone=time_zone, reminders=reminders, repeat=repeat,
+                priority=priority, sortOrder=sort_order, items=items,
+                # startDate=start_date, endDate=end_date, -- vidva hoceta tip parametra date
+            )
+
+            self.ticktick.createSubtask(task, parent_data["id"])
+
+            project = self.ticktick.getProjectById(task["projectId"])
+
+            embed = discord.Embed(
+                title=":ballot_box_with_check: TickTick New Task",
+                color=discord.Colour.from_str(
+                    project["color"] if project != {} else "#ffb301")
+            )
+
+            embed.set_thumbnail(
+                url="https://dashboard.snapcraft.io/site_media/appmedia/2022/02/icon_2XdTt7H.png"
+            )
+
+            embed.add_field(
+                name="Task ID", value=task["id"], inline=True)
+            embed.add_field(
+                name="Task title", value=task["title"], inline=True)
+            if project != {}:
+                embed.add_field(
+                    name="List name", value=project["name"], inline=True)
+            embed.add_field(
+                name="Task reminders", value=f"{len(task['reminders'])} reminders", inline=True)
+            embed.add_field(
+                name="Task subtasks", value=f"{len(task['items'])} reminders", inline=False)
+
+            await interaction.response.send_message(embed=embed)
 
     """
     Fix commented code
