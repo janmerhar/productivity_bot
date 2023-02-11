7ceee401990f650983e4fa86fdf95b1634ea8022
Jan Merhar <merharjan@gmail.com>
2023-02-11 01:38:33 +0100

    Add findAliases in classes/AliasFuncitons.py

--- classes/AliasFunctions.py
+++ classes/AliasFunctions.py
@@ -23,3 +23,16 @@ class AliasFunctions():
         self.mongo_commands = client["productivity_bot"]["custom_commands"]
         self.mongo_aliases = client["productivity_bot"]["aliases"]
 
+    def findAliases(self, identifier: str = "", n: int = 0):
+        res_command = self.mongo_aliases.find(
+            {"alias": {"$regex": identifier, "$options": "i"}}).limit(int(n)).sort(
+            "number_of_runs", -1)
+
+        return list(res_command)
+
+
+if __name__ == "__main__":
+    alias = AliasFunctions()
+    res = alias.findAliases("ptimers")
+
+    print(res)
