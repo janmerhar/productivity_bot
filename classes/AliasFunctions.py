aa13e9c91fd696c8a884da666a16634627fae916
Jan Merhar <merharjan@gmail.com>
2023-02-11 01:35:05 +0100

    Add classes/AliasFuncitons.py with class definition

---
+++ classes/AliasFunctions.py
@@ -0,0 +1,18 @@
+from ast import List
+import pymongo
+from pymongo import MongoClient
+from typing import Dict, Optional, Tuple, Union
+import requests
+from dateutil import parser
+import datetime
+import time
+import json
+from bson.objectid import ObjectId
+
+from dotenv import dotenv_values
+env = dotenv_values(".env")
+
+
+class AliasFunctions():
+    pass
+
