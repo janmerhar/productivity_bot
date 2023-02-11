327d024a38124d87b9e65f8c458cde2020adad69
Jan Merhar <merharjan@gmail.com>
2023-02-11 01:38:18 +0100

    Add constructor in classes/AliasFuncitons.py

--- classes/AliasFunctions.py
+++ classes/AliasFunctions.py
@@ -16,3 +16,10 @@ env = dotenv_values(".env")
 class AliasFunctions():
     pass
 
+    def __init__(self):
+        client = pymongo.MongoClient(
+            f"mongodb+srv://{env['MONGO_USERNAME']}:{env['MONGO_PASSWORD']}@cluster0.puvwbmu.mongodb.net/?retryWrites=true&w=majority")
+
+        self.mongo_commands = client["productivity_bot"]["custom_commands"]
+        self.mongo_aliases = client["productivity_bot"]["aliases"]
+
