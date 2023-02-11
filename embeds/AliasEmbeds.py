304ea316e81d63f7a80ca783a4bd8aea79f546ce
Jan Merhar <merharjan@gmail.com>
2023-02-11 02:22:44 +0100

    Add embeds/AliasEmbeds.py with class definition

---
+++ embeds/AliasEmbeds.py
@@ -0,0 +1,14 @@
+from embeds.TogglEmbeds import TogglEmbeds
+from re import A
+from typing import Dict, List, Optional
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+from classes.AliasFunctions import AliasFunctions
+from dotenv import dotenv_values
+env = dotenv_values(".env")
+
+
+class AliasEmbeds:
+    pass
