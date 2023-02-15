e0b13f168b4c1a6465989fd035312c19c6b3129c
Jan Merhar <merharjan@gmail.com>
2023-02-15 23:48:03 +0100

    Add TickTickEmbeds.newtask_embed() implementation in cogs/TickTick.py

--- cogs/TickTickCog.py
+++ cogs/TickTickCog.py
@@ -44,43 +44,10 @@ class TickTickCog(commands.Cog):
     # ATM used for creating new tasks
     @app_commands.command(name="newtask", description="TickTick add new task")
     async def newtask(self, interaction: discord.Interaction, name: str, project_id: str = None, content: str = None, desc: str = None, start_date: str = None, due_date: str = None, time_zone: str = None, reminders: str = None, repeat: str = None, priority: str = None, sort_order: str = None, items: str = None):
-        project_id = self.ticktick.getProject(project_id)
+        param = self.embeds.newtask_embed(name=name, project_id=project_id, content=content, desc=desc, start_date=start_date, due_date=due_date,
+                                          time_zone=time_zone, reminders=reminders, repeat=repeat, priority=priority, sort_order=sort_order, items=items)
 
-        if project_id == {} or project_id is None:
-            project_id = None
-        else:
-            project_id = project_id["id"]
-
-        task = self.ticktick.createTask(
-            title=name, projectId=project_id, content=content, desc=desc, timeZone=time_zone, reminders=reminders, repeat=repeat,
-            priority=priority, sortOrder=sort_order, items=items,
-            # startDate=start_date, endDate=end_date, -- vidva hoceta tip parametra date
-        )
-        project = self.ticktick.getProjectById(task["projectId"])
-
-        embed = discord.Embed(
-            title=":ballot_box_with_check: TickTick New Task",
-            color=discord.Colour.from_str(
-                project["color"] if project != {} else "#ffb301")
-        )
-
-        embed.set_thumbnail(
-            url="https://dashboard.snapcraft.io/site_media/appmedia/2022/02/icon_2XdTt7H.png"
-        )
-
-        embed.add_field(
-            name="Task ID", value=task["id"], inline=True)
-        embed.add_field(
-            name="Task title", value=task["title"], inline=True)
-        if project != {}:
-            embed.add_field(
-                name="List name", value=project["name"], inline=True)
-        embed.add_field(
-            name="Task reminders", value=f"{len(task['reminders'])} reminders", inline=True)
-        embed.add_field(
-            name="Task subtasks", value=f"{len(task['items'])} reminders", inline=False)
-
-        await interaction.response.send_message(embed=embed)
+        await interaction.response.send_message(**param)
 
     @app_commands.command(name="newsubtask", description="TickTick add new subtask")
     async def newsubtask(self, interaction: discord.Interaction, name: str, parent: str, project_id: str = None, content: str = None, desc: str = None, start_date: str = None, due_date: str = None, time_zone: str = None, reminders: str = None, repeat: str = None, priority: str = None, sort_order: str = None, items: str = None):
