ca90460f11b9f68c4e49d320ae2d7824818d8e15
Jan Merhar <merharjan@gmail.com>
2023-02-17 00:56:37 +0100

    Add TickTickEmbeds.deletetask_embed in embeds/TickTickEmbeds.py

--- embeds/TickTickEmbeds.py
+++ embeds/TickTickEmbeds.py
@@ -242,3 +242,45 @@ class TickTickEmbeds(EmbedsAbstract):
                 name="Task subtasks", value=f"{len(task_details['items'])} reminders", inline=False)
 
             return {"embeds": [embed]}
+
+    def deletetask_embed(self, name: str):
+        task = self.ticktick.deleteTask(name)
+        # print(task)
+
+        if task is None:
+            embed = discord.Embed(
+                title=":ballot_box_with_check: TickTick Delete Task",
+                color=0xffb301,
+                description="No task found"
+            )
+
+            embed.set_thumbnail(
+                url="https://dashboard.snapcraft.io/site_media/appmedia/2022/02/icon_2XdTt7H.png"
+            )
+
+            return {"embeds": [embed]}
+        else:
+            embed = discord.Embed(
+                title=":ballot_box_with_check: TickTick Delete Task",
+                color=0xffb301,
+            )
+
+            embed.set_thumbnail(
+                url="https://dashboard.snapcraft.io/site_media/appmedia/2022/02/icon_2XdTt7H.png"
+            )
+
+            # project = self.ticktick.getProjectById(task["projectId"])
+
+            embed.add_field(
+                name="Task ID", value=task["id"], inline=True)
+            embed.add_field(
+                name="Task title", value=task["title"], inline=True)
+            # embed.add_field(
+            #     name="List name", value=project["name"], inline=True)
+            embed.add_field(
+                name="Task reminders", value=f"{len(task['reminders'])} reminders", inline=True)
+            embed.add_field(
+                name="Task subtasks", value=f"{len(task['items'])} reminders", inline=False)
+
+            return {"embeds": [embed]}
+
