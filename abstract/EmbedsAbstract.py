c29d8dcffdc045e903f2e07ba805086cb1831bbc
Jan Merhar <merharjan@gmail.com>
2023-03-05 21:43:29 +0100

    Add EmbedsAbstract.createalias_method() and EmbedsAbstract.usealias_embed() abstract methods in abstract/EmbedsAbstract.py

--- abstract/EmbedsAbstract.py
+++ abstract/EmbedsAbstract.py
@@ -3,5 +3,11 @@ from abc import ABC, abstractmethod
 
 
 class EmbedsAbstract(ABC):
-    pass
+    @abstractmethod
+    def createalias_embed(self, command: str,  alias: str, arguments: str = "", cog_param: object = {}) -> dict:
+        pass
+
+    @abstractmethod
+    def usealias_embed(self, alias: str):
+        pass
 
