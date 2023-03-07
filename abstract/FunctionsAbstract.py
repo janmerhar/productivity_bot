9775997dfe0eb0528b6426d3ae0af45a6014c886
Jan Merhar <merharjan@gmail.com>
2023-03-07 16:53:09 +0100

    Add FunctionsAbstract.findSavedShortcut() in abstract/FunctionsAbstract.py

--- abstract/FunctionsAbstract.py
+++ abstract/FunctionsAbstract.py
@@ -6,3 +6,9 @@ class FunctionsAbstract(ABC):
     @abstractmethod
     def saveShortcut2(self, command: str, alias: str, param: object = {}):
         pass
+
+    def findSavedShortcut(self, alias: str):
+        saved_shortcut = self.mongo_aliases.find_one({"alias": alias})
+
+        return saved_shortcut
+
