adff9914e033c716d0f27f9c9ed472c6a506795a
Jan Merhar <merharjan@gmail.com>
2023-03-09 19:55:03 +0100

    Add AliasEmbeds.usealias_embed() in embeds/AliasEmbeds.py

--- embeds/AliasEmbeds.py
+++ embeds/AliasEmbeds.py
@@ -21,3 +21,31 @@ class AliasEmbeds:
         self.embed_classes = {"toggl": self.toggl_embeds,
                               "ticktick": self.ticktick_embeds}
 
+    def usealias_embed(self, alias: str):
+        # Iskanje, ce alias obstaja
+        find_alias = self.alias.findAliases(identifier=alias)
+
+        if len(find_alias) > 0:
+            find_alias = find_alias[0]
+
+            # Iskanje po embed_classes, ce obstaja komanda
+            # Tale if stavek gre notri, toda retun ne dela
+            if find_alias["application"] in self.embed_classes:
+                # TogglEmbeds object
+                alias_class = self.embed_classes[find_alias["application"]]
+
+                return alias_class.usealias_embed(alias=alias)
+
+        # Nismo nasli alias
+        # tukaj bom samo na koncu narredi en embed return
+
+        embed_no_found = discord.Embed(
+            title=":stopwatch: New alias",
+            color=discord.Colour.from_str("#ff0000"),
+            description="Alias command not found"
+        )
+        embed_no_found.set_thumbnail(
+            url="https://i.imgur.com/Cmjl4Kb.png"
+        )
+
+        return {"embeds": [embed_no_found]}
