2fab1778b9b752644c5e29c990162b9e4ddbb51a
Jan Merhar <merharjan@gmail.com>
2023-03-22 19:28:58 +0100

    Add command descriptions and renames for all Tasks in cogs/TickTickCog.py

--- cogs/TickTickCog.py
+++ cogs/TickTickCog.py
@@ -41,8 +41,25 @@ class TickTickCog(commands.Cog):
     # Tasks
     #
 
-    # ATM used for creating new tasks
     @app_commands.command(name="newtask", description="TickTick add new task")
+    @app_commands.describe(
+        name="Name of the added task",
+        project_id="Project of the task",
+        content="Content of the task",
+        desc="Description of the task",
+        start_date="Start of the task",
+        due_date="Due date of the task",
+        time_zone="Task time zone",
+        reminders="List of reminders",
+        repeat="Does task repeat",
+        priority="Priority of the task",
+        sort_order="Order by which the task will be sorted",
+        items="Subtasks of the task",
+    )
+    @app_commands.rename(
+        desc="description",
+        items="subtasks",
+    )
     async def newtask(self, interaction: discord.Interaction, name: str, project_id: str = None, content: str = None, desc: str = None, start_date: str = None, due_date: str = None, time_zone: str = None, reminders: str = None, repeat: str = None, priority: str = None, sort_order: str = None, items: str = None):
         param = self.embeds.newtask_embed(name=name, project_id=project_id, content=content, desc=desc, start_date=start_date, due_date=due_date,
                                           time_zone=time_zone, reminders=reminders, repeat=repeat, priority=priority, sort_order=sort_order, items=items)
@@ -50,6 +67,25 @@ class TickTickCog(commands.Cog):
         await interaction.response.send_message(**param)
 
     @app_commands.command(name="newsubtask", description="TickTick add new subtask")
+    @app_commands.describe(
+        name="Name of the added task",
+        parent="Parent task",
+        project_id="Project of the task",
+        content="Content of the task",
+        desc="Description of the task",
+        start_date="Start of the task",
+        due_date="Due date of the task",
+        time_zone="Task time zone",
+        reminders="List of reminders",
+        repeat="Does task repeat",
+        priority="Priority of the task",
+        sort_order="Order by which the task will be sorted",
+        items="Subtasks of the task",
+    )
+    @app_commands.rename(
+        desc="description",
+        items="subtasks",
+    )
     async def newsubtask(self, interaction: discord.Interaction, name: str, parent: str, project_id: str = None, content: str = None, desc: str = None, start_date: str = None, due_date: str = None, time_zone: str = None, reminders: str = None, repeat: str = None, priority: str = None, sort_order: str = None, items: str = None):
         param = self.embeds.newsubtask_embed(name=name, parent=parent, project_id=project_id, content=content, desc=desc, start_date=start_date,
                                              due_date=due_date, time_zone=time_zone, reminders=reminders, repeat=repeat, priority=priority, sort_order=sort_order, items=items)
@@ -57,12 +93,34 @@ class TickTickCog(commands.Cog):
         await interaction.response.send_message(**param)
 
     @app_commands.command(name="complete", description="TickTick complete a task")
+    @app_commands.describe(
+        name="Task to be completed"
+    )
     async def complete(self, interaction: discord.Interaction, name: str):
         param = self.embeds.complete_embed(name=name)
 
         await interaction.response.send_message(**param)
 
     @app_commands.command(name="updatetask", description="TickTick update a task")
+    @app_commands.describe(
+        identifier="Task to be updated",
+        name="Name of the added task",
+        project_id="Project of the task",
+        content="Content of the task",
+        desc="Description of the task",
+        start_date="Start of the task",
+        due_date="Due date of the task",
+        time_zone="Task time zone",
+        reminders="List of reminders",
+        repeat="Does task repeat",
+        priority="Priority of the task",
+        sort_order="Order by which the task will be sorted",
+        items="Subtasks of the task",
+    )
+    @app_commands.rename(
+        desc="description",
+        items="subtasks",
+    )
     async def updatetask(self, interaction: discord.Interaction, identifier: str, name: str = None, project_id: str = None, content: str = None, desc: str = None, start_date: str = None, due_date: str = None, time_zone: str = None, reminders: str = None, repeat: str = None, priority: str = None, sort_order: str = None, items: str = None):
         param = self.embeds.updatetask_embed(identifier=identifier, name=name, project_id=project_id, content=content, desc=desc, start_date=start_date,
                                              due_date=due_date, time_zone=time_zone, reminders=reminders, repeat=repeat, priority=priority, sort_order=sort_order, items=items)
@@ -70,6 +128,13 @@ class TickTickCog(commands.Cog):
         await interaction.response.send_message(**param)
 
     @app_commands.command(name="movetask", description="TickTick move a task to other list")
+    @app_commands.describe(
+        task_details="Task to be moved",
+        list="Destination list of the moved task",
+    )
+    @app_commands.rename(
+        task_details="name"
+    )
     async def movetask(self, interaction: discord.Interaction, task_details: str, list: str):
         param = self.embeds.movetask_embed(
             task_details=task_details, list=list)
@@ -77,12 +142,21 @@ class TickTickCog(commands.Cog):
         await interaction.response.send_message(**param)
 
     @app_commands.command(name="deletetask", description="TickTick delete task")
+    @app_commands.describe(
+        name="Task to be deleted"
+    )
     async def deletetask(self, interaction: discord.Interaction, name: str):
         param = self.embeds.deletetask_embed(name=name)
 
         await interaction.response.send_message(**param)
 
     @app_commands.command(name="getlist", description="TickTick get list")
+    @app_commands.describe(
+        identifier="List to be retrieved"
+    )
+    @app_commands.rename(
+        identifier="name"
+    )
     async def getlist(self, interaction: discord.Interaction, identifier: str):
         param = self.embeds.getlist_embed(identifier=identifier)
 
