11e3a258a5c4f0b8dfc5f336cef701be66841d88
Jan Merhar <merharjan@gmail.com>
2026-02-14 18:30:19 +0100

    update UserSettings.py

--- classes/UserSettings.py
+++ classes/UserSettings.py
@@ -6,6 +6,7 @@ from typing import Any, Dict, Optional
 class UserSettings:
     user_id: int
     timezone: Optional[str] = None
+    toggl_api_keys: Optional[Dict[str, str]] = None
     created_at: Optional[str] = None
     updated_at: Optional[str] = None
 
@@ -17,6 +18,7 @@ class UserSettings:
         return UserSettings(
             user_id=int(document.get("user_id", user_id)),
             timezone=document.get("timezone"),
+            toggl_api_keys=document.get("toggl_api_keys"),
             created_at=document.get("created_at"),
             updated_at=document.get("updated_at"),
         )
@@ -25,6 +27,7 @@ class UserSettings:
         return {
             "user_id": self.user_id,
             "timezone": self.timezone,
+            "toggl_api_keys": self.toggl_api_keys,
             "created_at": self.created_at,
             "updated_at": self.updated_at,
         }
